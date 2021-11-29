ad4702646cf1c1fbda749b47c9146bf19d16d977
williamwhang <whang.william@gmail.com>
2021-11-29 15:06:08 -0300

    Ajuste 2

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -18,15 +18,13 @@ function Project() {
 
       <section>
         <ProjectItem 
-          img="https://user-images.githubusercontent.com/73802190/143438835-1cae53d3-7707-4de0-9505-31e5941dc405.jpg"
-          title={''}
-          type={''}
-          slug={''} 
+         img="https://user-images.githubusercontent.com/73802190/143438835-1cae53d3-7707-4de0-9505-31e5941dc405.jpg" title={''} type={''} slug={''}          // title="DH BURGUER"
+          // type="This project was developed during the Full-Stack course at Digital House, integration project." 
+          // slug={''}          // slug="teste"
         />
         
         <ProjectItem2
-          img="https://user-images.githubusercontent.com/73802190/143436963-deaa5d63-08b2-4541-bb94-3ca94c4499a6.jpg"
-          // title="IGNEWS - Portal de notícias"
+         img="https://user-images.githubusercontent.com/73802190/143436963-deaa5d63-08b2-4541-bb94-3ca94c4499a6.jpg" title={''} type={''} slug={''}          // title="IGNEWS - Portal de notícias"
           // type="This project was developed during Rocketseat Ignite! The idea was to develop a paid blog where 
           // the user logs in using GitHub and pays their subscription using Stripe." 
           // slug={'https://github.com/williamwhang/primeiro-projeto-next'}          // slug="teste"
@@ -34,6 +32,7 @@ function Project() {
         
         {/* <ProjectItem3 -criar arquivo e importar 
           img="https://zippypixels.com/wp-content/uploads/2015/09/01-Free-perspective-website-mockup-824x542.jpg"
+
         />
          */}
 
